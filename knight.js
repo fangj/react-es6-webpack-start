77ab01c4e69e34270b866ebbb74363c8655ce301
Fang Jian <fangjianmail@126.com>
2016-03-19 20:52:07 +0800

    extract css ok

--- knight.js
+++ knight.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { decorator, transformer } from './pacomo';
-require('./Knight.less');
+require('./Knight.css');
 
 @decorator
 export default class Knight extends Component {
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 var webpack = require("webpack");
-
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports={
   entry:{
     index:'./index.js',
@@ -19,10 +19,16 @@ module.exports={
       {
         test: /\.less$/,
         loader: "style!css!less"
+      },
+       // Extract css files
+      {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract("style-loader", "css-loader")
       }
     ]
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' })
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js' }),
+    new ExtractTextPlugin("[name].css")
   ]
 }
